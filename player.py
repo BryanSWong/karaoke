494ffa94e3b8857033f194420fa592b3244006a6
Bryan Wong <bwong727@gmail.com>
2016-05-06 00:11:33 -0400

    updates to player class to python 3.5 standards

--- player.py
+++ player.py
@@ -5,16 +5,15 @@ class Player:
         self.queue = set()
 
     def play(self):
-        print "Now playing: '%s' by %s, official video at %s" % (self.song.gettitle(), self.song.getartist(), self.song.getlink())
-        # return is used instead of print to avoid a none in the console
+        print ("Now playing: '%s' by %s, official video at %s" % (self.song.gettitle(), self.song.getartist(), self.song.getlink()))
 
     def add(self, song):
         self.queue.add(song)
-        print "Song '%s' added to playlist" % (song.gettitle())
+        print ("Song '%s' added to playlist" % (song.gettitle()))
 
     def remove(self, song):
         self.queue.remove(song)
-        print "Song '%s' removed from playlist" % (song.gettitle())
+        print ("Song '%s' removed from playlist" % (song.gettitle()))
 
     def queue_play(self):
         for song in self.queue:
