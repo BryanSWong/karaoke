73a6a82493f2a0ac877a94775d5c4660a962c2f8
Bryan Wong <bwong727@gmail.com>
2016-05-18 01:11:42 -0400

    code update to interface.py

--- interface.py
+++ interface.py
@@ -1,54 +1,81 @@
 from sys import exit
 
 
-class Choice:
+class Interface(object):
+
+    def __init__(self, the_menu):
+        self.the_menu = the_menu
+
+    def start(self):
+        current_menu = self.the_menu.opening_menu()
+        last_menu = self.the_menu.next_menu('off')
+
+        while current_menu != last_menu:
+            next_menu_choice = current_menu.display
+            current_menu = self.the_menu.next_menu(next_menu_choice)
+
+
+class Main:
+
+    @property
+    def display(self):
+        print("Welcome to the main menu please select a option:")
+        print("1. Play a song.")
+        print("2. Create and play a playlist.")
+        print("3. Power off.")
+
+        action = raw_input("> ")
+
+        if action == '1':
+            print("programing needed")
+            return 'main'
+
+        elif action == '2':
+            print("programing needed")
+            return 'main'
+
+        elif action == '3':
+            print("Powering off, have a nice day.")
+            return 'off'
+
+        else:
+            print("That is not a valid option.")
+            return 'main'
+
+
+class Off:
 
     def display(self):
-        print "This menu is not yet configured."
         exit(1)
 
 
-class Interface:
-
-    def __init__(self, menu):
-        self.menu = menu
-
-    def start(self):
-        current_menu = self.menu.current_menu()
-        power_off = self.menu.next_menu('off')
-
-        while current_menu != power_off:
-            next_menu_name = current_menu.display()
-            current_menu = self.menu.next_menu(next_menu_name)
-
-        current_menu.display()
-
-
-class Main(Choice):
+class Play:
 
     def display(self):
         pass
 
 
-class Off(Choice):
+class Queue:
 
     def display(self):
         pass
 
 
-class Power:
+class Power(object):
 
     menu = {
         'main': Main(),
         'off': Off(),
+        'play': Play(),
+        'queue': Queue(),
     }
 
     def __init__(self, start_menu):
         self.start_menu = start_menu
 
-    def next_menu(self, scene_name):
-        val = Power.menu.get(scene_name)
+    def next_menu(self, menu_name):
+        val = Power.menu.get(menu_name)
         return val
 
-    def current_menu(self):
+    def opening_menu(self):
         return self.next_menu(self.start_menu)
