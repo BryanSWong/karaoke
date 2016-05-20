cf226ab14624c636da26dc999e3cdfce7a1184c3
Bryan Wong <bwong727@gmail.com>
2016-05-20 16:23:45 -0400

    Code updates

--- interface.py
+++ interface.py
@@ -1,6 +1,21 @@
 from sys import exit
 from player import Player
-from songDB import songDB
+from songDB import songDB, song
+starter = Player(song)  # a var that sets up the player class for later use.
+
+
+def options(songlist):
+    count = 1
+    for song in songlist:
+        print("%d.Title: %s, Artist: %s: Link:%s") % (count, song.get_title(), song.get_artist(), song.get_link())
+        count += 1
+
+
+def queuelist(thequeue):
+    count = 1
+    for item in thequeue:
+        print("%d.Title: %s, Artist: %s: Link:%s") % (count, item.get_title(), item.get_artist(), item.get_link())
+        count += 1
 
 
 class Interface:
@@ -31,7 +46,7 @@ class Main:
 
         elif action == '2':
             print("programing needed")
-            return 'main'
+            return 'playlist'
 
         elif action == '3':
             print("Powering off, have a nice day.")
@@ -53,9 +68,7 @@ class Play:
     def display(self):
         count = 1
         print("Which song do you want to play:")
-        for song in songDB:
-            print("%d.Title: %s, Artist: %s: Link:%s") % (count, song.get_title(), song.get_artist(), song.get_link())
-            count += 1
+        options(songDB)
         print("type main to go back to main menu")
 
         action = raw_input("> ")
@@ -105,8 +118,43 @@ class Play:
 
 
 class Queue:
+    @property
     def display(self):
-        pass
+        print("The playlist is currently empty.")
+        print("Which song do you want to add to the playlist?")
+        options(songDB)
+        print("type main to go back to main menu")
+
+        action = raw_input("> ")
+        action.lower()
+
+        if action == 'main':
+            return 'main'
+
+        elif action == '1':
+            starter.add(songDB[0])
+            return 'queue'
+
+        elif action == '2':
+            starter.add(songDB[1])
+            return 'queue'
+
+        elif action == '3':
+            starter.add(songDB[2])
+            return 'queue'
+
+        elif action == '4':
+            starter.add(songDB[3])
+            return 'queue'
+
+        elif action == '5':
+            starter.add(songDB[4])
+            return 'queue'
+
+        else:
+            print("That is not a valid option.")
+            print("")
+            return 'playlist'
 
 
 class Again:
@@ -131,13 +179,115 @@ class Again:
             return 'again'
 
 
+class Playlist:
+    @property
+    def display(self):
+        print("Current song in playlist:")
+        count = 1
+        for item in starter.queue:
+            print("%d.Title: %s, Artist: %s, Link: %s") % (count, item.get_title(), item.get_artist(), item.get_link())
+            count += 1
+        print("Would you like to 'play the list now?")
+        print("type in 'play'.")
+        print("if you want to add more songs type in 'add'.")
+        print("fi you want to remove a song type in 'remove'.")
+        print("if you want to return to the main menu type in 'main'.")
+        print("note if you return to main your playlist will be deleted.")
+
+        action = raw_input("> ")
+        action.lower()
+
+        if action == 'main':
+            starter.queue.clear()  # clears the queue.
+            return 'main'
+
+        elif action == 'add':
+            return 'add'
+
+        elif action == 'remove':
+            return 'remove'
+
+        else:
+            print("That is not a valid option.")
+            print("")
+            return 'queue'
+
+
+class Add:
+    @property
+    def display(self):
+        print("Which other song do you want to add?")
+        options(songDB)
+        print("type in main to return to main menu, also discards your playlist.")
+        print("note: repeated songs will not be played so only add one of each.")
+
+        action = raw_input("> ")
+        action.lower()
+
+        if action == 'main':
+            starter.queue.clear()
+            return 'main'
+
+        elif action == '1':
+            starter.add(songDB[0])
+            return 'queue'
+
+        elif action == '2':
+            starter.add(songDB[1])
+            return 'queue'
+
+        elif action == '3':
+            starter.add(songDB[2])
+            return 'queue'
+
+        elif action == '4':
+            starter.add(songDB[3])
+            return 'queue'
+
+        elif action == '5':
+            starter.add(songDB[4])
+            return 'queue'
+
+        else:
+            print("That is not a valid option.")
+            print("")
+            return 'playlist'
+
+
+class Remove:
+    @property
+    def display(self):
+        print("Which song do you want to remove?")
+        queuelist(starter.queue)
+        print("type main to return to the main menu.")
+        print("note: returning to the main menu will discard your playlist.")
+
+        action = raw_input("> ")
+        action.lower()
+
+        if action == 'main':
+            starter.queue.clear()
+            return 'main'
+
+        elif action == '1':
+            print("programming needed to work.")
+            return 'remove'
+
+        else:
+            print("That is not a valid choice.")
+            return 'remove'
+
+
 class Power:
     menu = {
         'main': Main(),
         'off': Off(),
         'play': Play(),
-        'queue': Queue(),
+        'queue': Playlist(),
         'again': Again(),
+        'playlist': Queue(),
+        'add': Add(),
+        'remove': Remove(),
     }
 
     def __init__(self, start_menu):
--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 from interface import Power, Interface
 
-# print(song.get_title())
-# print(song.get_artist())
-# print(song.get_link())
-
-# ten.add(song)
-# listen.add(song2)
-# listen.add(song3)
-# listen.add(song4)
-# listen.add(song5)
-
-# listen.play()
-# listen.queue_play()
-
-# listen.remove(song2)
-
-# listen.queue_play()
-
 power_on = Power('main')
 start = Interface(power_on)
 start.start()
